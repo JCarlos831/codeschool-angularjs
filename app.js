36fb6af786da611bade8b0e70b20a27d4aafb346
Juan Montoya <jcmontoya@gmail.com>
2018-02-04 21:27:13 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/* global angular */
+(function() {
+    var app = angular.module('store', []);
+    
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+    
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: '...',
+        canPurchase: false
+    }
+})();
