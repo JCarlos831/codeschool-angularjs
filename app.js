13577362321f20899c53ec658432811513c9b290
Juan Montoya <jcmontoya@gmail.com>
2018-02-06 00:46:34 +0000

    Level 2 completed

--- app.js
+++ app.js
@@ -3,13 +3,34 @@
     var app = angular.module('store', []);
     
     app.controller('StoreController', function(){
-        this.product = gem;
+        this.products = gems;
     });
     
-    var gem = {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: '...',
-        canPurchase: false
-    }
+    var gems = [
+        {
+            name: "Dodecahedron",
+            price: 2,
+            description: "Dodecahedron Description",
+            canPurchase: true,
+            soldOut: true
+        },
+        {
+            name: "Pentagonal Gem",
+            price: 5.95,
+            description: "Pentagonal Gem Description",
+            canPurchase: false
+        }
+    ];
+    
+    app.controller('PanelController', function(){
+        this.tab = 1;
+        
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+    });
 })();
