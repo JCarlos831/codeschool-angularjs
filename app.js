8b0ca44e556f4dff1da19ca6476886697d1d1d52
Juan Montoya <jcmontoya@gmail.com>
2018-02-07 01:14:04 +0000

    Start of car rental project

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 /* global angular */
-(function() {
+(function(){
     var app = angular.module('store', []);
     
-    app.controller('StoreController', function(){
-        this.products = gems;
+    app.controller('RentalController', function(){
+        this.products = cars;
     });
     
-    var gems = [
-        {
-            name: "Dodecahedron",
-            price: 2,
-            description: "Dodecahedron Description",
-            canPurchase: true,
-            soldOut: true
-        },
-        {
-            name: "Pentagonal Gem",
-            price: 5.95,
-            description: "Pentagonal Gem Description",
-            canPurchase: false
-        }
+    var cars = [
+    {
+        year: 2018,
+        make: 'Dodge',
+        model: 'Challenger',
+        type: 'Coupe',
+        description: 'Everyone will tell you not to mess with an icon. But the Dodge Brand has never listened to the “everyones” of the world.\nSo they took this model of muscle—the Dodge Challenger—and, in true Dodge Brand fashion, made it even better.',
+        price: 79.99,
+        available: true,
+        unavailable: false
+    },
+    {
+        year: 2018,
+        make: 'Chevy',
+        model: 'Camaro',
+        type: 'Coupe',
+        description: 'With a profile that harkens back to the 1967 original, the all-newsixth-generation Camaro looks pleasingly familiar, marking\na subtle styling evolution over the previous model. But don’t let that fool you. The new Camaro is a delight to drive, with dynamic\nprowess that can give even snooty European sportsters a run for their money. But as a daily driver, you might want to think twice\nbecause outward visibility is atrocious.',
+        price: 69.99,
+        available: true,
+        unavailable: false
+    }
     ];
     
-    app.controller('PanelController', function(){
-        this.tab = 1;
-        
-        this.selectTab = function(setTab) {
-            this.tab = setTab;
-        };
-        
-        this.isSelected = function(checkTab) {
-            return this.tab === checkTab;
-        };
-    });
 })();
